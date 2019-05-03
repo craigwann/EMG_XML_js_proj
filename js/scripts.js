a93230da61cf1bd211cb5b73fa90136538ddf2c0
Craig Wann <cwann@EMS.local>
2019-05-03 13:31:52 -0700

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+console.log("remove comment lines");
+//  &#92; --> backslash 
+console.log("remove structure indents and extra returns");
+console.log("remove returns between nested table tags");
+function buttonFunction() {
+    alert("This project is in progress.")
+}
+$(document).ready(function () {
+    $("h1").click(function () {
+        alert("Englund Marine & Industrial Supply Co. Inc., U.S.Distibuting Inc. and Marine Wholesale Inc");
+    });
+});
